45fa71bb6f9dd33a2596a35b8d036b880c6325e2
Jefferson Alaiza <jeffersonaalaiza@iskolarngbayan.pup.edu.ph>
2025-12-26 22:32:56 +0800

    init

---
+++ Application/Courses/Commands/CreateCourseCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Courses.Commands
+{
+    public record CreateCourseCommand(string Title, string Description) : IRequest;
+}
